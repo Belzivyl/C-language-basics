9e4ece73134325d543761ebf192f94f41f5cf4da
Belzivyl <artemsotonin@gmail.com>
2020-09-24 17:49:26 +0500

    Сделал первое домашнее задание пл теме Основы языка C#.

---
+++ HomeWork1/Exercise №3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise__3
+{
+    #region Сотонин Артем - Условие к заданию №3
+    ////а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2 по формуле r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2).
+    //Вывести результат, используя спецификатор формата .2f(с двумя знаками после запятой);
+    //б) * Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+    #endregion
+    class Program
+    {
+        static void Computation(double x1, double y1, double x2, double y2) 
+        {
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine($"Растояние между двумя точками равно: {r:f2}.");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте вас приветсвует программа по подсчету рассояния между двумя точками\nВведите координаты первой точки по оси X.");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты первой точки по оси Y.");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты второй точки по оси X.");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты второй точки по оси Y.");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine($"Растояние между двумя точками равно: {r:f2}.");
+
+            Computation(x1, y1, x2, y2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    #region Сотонин Артем - Условие к заданию №1
+//    Написать программу «Анкета». Последовательно задаются вопросы(имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку:
+//    а) используя склеивание;
+//    б) используя форматированный вывод;
+//	  в) используя вывод со знаком $.
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Зжравствуйте, вас приветствует программа \"Анкета\".\nПожалуйста заолните свою анкету отвечая на вопросы и выполняя зажания.\nКак вас зовут?");
+            String firstName =Console.ReadLine();
+
+            Console.WriteLine("Назовите свою фамилию.");
+            String secondName = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            String age = Console.ReadLine();
+
+            Console.WriteLine("Какой у вас рост в метрах?");
+            String growth = Console.ReadLine();
+
+            Console.WriteLine("Сколько вы весите в килограммах?");
+            String weight = Console.ReadLine();
+
+            Console.WriteLine("Ваше имя и фамилия: " + firstName + " " + secondName + ". Вам " + age + " лет. Ваш рост: " + growth + " метра. Ваш вес: " + weight + " килограмм.");
+
+            Console.WriteLine("Ваше имя и фамилия: {0} {1}. Вам {2} лет. Ваш рост: {3} метра. Ваш вес: {4} килограмм.", firstName, secondName, age, growth, weight);
+
+            Console.WriteLine($"Ваше имя и фамилия: {firstName} {secondName}. Вам {age} лет. Ваш рост: {growth} метра. Ваш вес: {weight} килограмм.");
+
+            Console.ReadKey();
+        }
+    }
+}
